d0f063e3d63b72ee4d7dd72369f8de031991aa3d
IDXTests <79197219+IDXTests@users.noreply.github.com>
2021-02-17 12:29:25 +0200

    small fix

--- df.js
+++ df.js
@@ -1,2 +1,2 @@
-this.engine.factory.scriptEngine.eval('load("https://gigyaidx.blob.core.windows.net/idx-test/idan/df-esc.js");'); 
+this.engine.factory.scriptEngine.eval('load("https://github.com/IDXTests/IDXTests/blob/master/df-esc.js");'); 
 //console.log("Hello, World!");

